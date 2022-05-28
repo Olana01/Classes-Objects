16cd9f5751f6df09989379a33e34d1d8c01bc9c6
unknown <opeyemilana01@gmail.com>
2022-05-28 19:32:26 +0100

    first commit

---
+++ main.py.py
@@ -0,0 +1,36 @@
+class Student:
+    # [assignment] Skeleton class. Add your code here
+    def __init__(self, name, age, tracks, score):
+        self.name = str(name)
+        self.age = int(age)
+        self.tracks = list(tracks)
+        self.score = float(score)
+
+    def change_name(self, new_name):
+        self.name = new_name
+        new_name = "Peter"
+        print(f"the student updated his name to {new_name}")
+
+    def change_age(self, new_age):
+        self.age = new_age
+        new_age = "34"
+        print(f"the student updated his age to {new_age}")
+        
+    def add_track(self, new_track):
+        self.track = new_track
+        new_track = "UI/UX"
+        print(f"the student updated his track to {new_track}")
+
+    def get_score(self, new_score):
+        self.score = new_score
+        new_score = "25.50"
+        print(f"the student updated his score to {new_score}")    
+
+
+Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
+
+# Expected methods
+Bob.change_name("Peter")
+Bob.change_age(34)
+Bob.add_track("UI/UX")
+Bob.get_score(25.50)
